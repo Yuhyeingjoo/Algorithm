f73c4355ee2fa2c58809baa3c5e3c0397bdc904d
YuHyeongJoo <21700473@handone.edu>
2021-03-03 19:43:14 +0900

    1_2 problem

---
+++ 1_2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int sum_of_num(string s1){
+	int sum1=0;
+
+	for(int i=0; i<s1.size(); i++){
+		if(s1[i]>48 &&s1[i]<58)
+			sum1 = sum1+s1[i]-48;
+	}
+	return sum1;
+	
+}
+bool comp(string s1, string s2){
+	if(s1.size()<s2.size())
+		return s1.size()<s2.size();
+	else if(s1.size()== s2.size()){
+		if(sum_of_num(s1)<sum_of_num(s2))
+			return true;
+		else
+			return false;
+	}
+	else
+		return s1<s2;
+	
+}
+
+int main(){
+	int n;
+	string input;
+	cin>>n;
+	vector<string> v;
+	for(int i=0; i<n ;i++){
+		cin>>input;
+		v.push_back(input);
+
+	}
+	sort(v.begin(),v.end(),comp);
+	
+	for(int i=0; i<v.size(); i++){
+		cout<<v[i]<<endl;
+	}
+}
