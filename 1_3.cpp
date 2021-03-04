6644bfdacbf424290ff5cbd1c0d280defb33c62c
YuHyeongJoo <21700473@handone.edu>
2021-03-05 00:55:19 +0900

    secont 1_3

--- 1_3.cpp
+++ 1_3.cpp
@@ -16,32 +16,38 @@ bool comp(int* a, int* b){
 	else
 		return true;
 }
+int find(int n, vector<int*> v){
+	for(int i=0; i<v.size(); i++){
+		if(v[i][1]==n)
+			return i;
+	}
+	return -1;
+}
 int main(){
 	int n , c, input;
 	cin>>n>>c;
 	int* arr  = (int*)malloc(sizeof(int)*3);
 	arr[0]= -1;
-	vector<int*> v(c+1, arr);
+	vector<int*> v;
 	for(int i=0; i<n; i++){
 		cin>>input;
-		if(v[input][0]==  -1){
+		if(find(input,v)==-1){
+			cout<<"ureca"<<endl;
 			int* tmp  = (int*)malloc(sizeof(int)*3);
 			tmp[0] = 1;
 			tmp[1] = input;
 			tmp[2] = i;
-			v[input] = tmp;
+			v.push_back(tmp);
 		}
-		else{
-			v[input][0] +=1;
-		}
+		else	
+			v[find(input,v)][0] +=1;
+		
 	}
 		sort(v.begin(), v.end(),comp);
 	
-	for(int i=0; i<=c; i++){
-		if(v[i][0]!=-1){
-			for(int j=0; j<v[i][0]; j++)
-				cout<<v[i][1]<<" ";
-		}
+	for(int i=0; i<v.size(); i++){
+		for(int j=0; j<v[i][0]; j++)
+			cout<<v[i][1]<<" "<<i<<endl;;
 	}
 
 
