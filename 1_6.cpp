e25d5610edf8efde4912cc50ac57c279216394c7
YuHyeongJoo <21700473@handone.edu>
2021-03-05 15:56:52 +0900

    1_6.cpp first

--- 1_6.cpp
+++ 1_6.cpp
@@ -3,19 +3,29 @@
 #include<vector>
 using namespace std;
 bool visit[9];
-int arr[9];
+int arr[9],pre[9];
 vector<int> v;
 int n,m;
+bool is_repeat(){
+	for(int i=1; i<=m; i++){
+		if(arr[i] !=pre[i])
+			return false;
+	}
+	return true;
+}
 void dfs(int s){
 		if(s==m+1){
 			for(int i=1; i<=m; i++){
 				cout<<arr[i]<<" ";
 			}
 			cout<<"\n";
+
+			
 			return;
 		}
 	for(int i = 1 ;  i <=n; i++){
-		if(!visit[i]){
+		if(!visit[i]&& pre[s]!=v[i-1]){
+			pre[s] = v[i-1];
 			visit[i] = true;
 			arr[s] = v[i-1];
 			dfs(s+1);
